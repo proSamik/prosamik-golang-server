be12b3344decf04a0a0dd4d98b2527e09b9dfda5
proSamik <samikchoudhury15@gmail.com>
2024-12-27 23:01:42 +0530

    refactor- removed the validation of gh token

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
 	"log"
 	"net/http"
-	"prosamik-backend/internal/auth"
 	"prosamik-backend/internal/router"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	// Validate GitHub token
-	if err := auth.ValidateGitHubToken(); err != nil {
-		log.Fatalf("Authentication error: %v", err)
-	}
 
 	// Setup routes
 	router.SetupRoutes()
--- internal/auth/github_auth.go
+++ internal/auth/github_auth.go
@@ -1,20 +1,12 @@
 package auth
 
 import (
-	"errors"
 	"os"
+	"strings"
 )
-
-// ValidateGitHubToken checks if GitHub token is set
-func ValidateGitHubToken() error {
-	token := os.Getenv("GITHUB_TOKEN")
-	if token == "" {
-		return errors.New("GITHUB_TOKEN environment variable is not set")
-	}
-	return nil
-}
 
 // GetGitHubToken retrieves the GitHub token
 func GetGitHubToken() string {
-	return os.Getenv("GITHUB_TOKEN")
+	token := os.Getenv("GITHUB_TOKEN")
+	return strings.TrimSpace(token) // Removes extra spaces and newlines
 }
